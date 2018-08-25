fdd6efc47a8bce626eb1bc79d706b87dd827a99b
JL-Z <zhangjl_0912@163.com>
2018-08-25 09:58:32 +0800

    Commit

--- zhihu/zhihu/settings.py
+++ zhihu/zhihu/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'zhihu.spiders'
 #USER_AGENT = 'zhihu (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
